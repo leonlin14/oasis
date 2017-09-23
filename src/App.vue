46a43f2e8968182a8131850ed4f13ba6273c5378
leonlin <leonlin14@gmail.com>
2017-09-23 12:02:23 +0800

    update menu

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,49 @@
 <template>
     <v-app id="app" toolbar footer>
-        <v-navigation-drawer
-            persistent
-            v-model="drawer"
-            light
-            enable-resize-watcher
-            absolute
-        >
-            <v-list dense>
-                <v-list-tile @click="">
+        <v-navigation-drawer absolute persistent light :mini-variant.sync="mini" v-model="drawer" overflow>
+            <v-toolbar flat class="transparent">
+                <v-list class="pa-0">
+                    <v-list-tile avatar>
+                        <v-list-tile-avatar>
+                            <img src="https://randomuser.me/api/portraits/women/82.jpg" />
+                        </v-list-tile-avatar>
+                        <v-list-tile-content>
+                            <v-list-tile-title>Eva Williamson</v-list-tile-title>
+                        </v-list-tile-content>
+                        <v-list-tile-action>
+                            <v-btn icon @click.native.stop="mini = !mini">
+                                <v-icon>chevron_left</v-icon>
+                            </v-btn>
+                        </v-list-tile-action>
+                    </v-list-tile>
+                </v-list>
+            </v-toolbar>
+            <v-list class="pt-0" dense>
+                <v-divider></v-divider>
+                <v-list-tile v-for="item in items" :key="item.title" @click="">
                     <v-list-tile-action>
-                        <v-icon>home</v-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
                     </v-list-tile-action>
                     <v-list-tile-content>
-                        <v-list-tile-title>Home</v-list-tile-title>
+                        <v-list-tile-title>{{ item.title }}</v-list-tile-title>
                     </v-list-tile-content>
                 </v-list-tile>
             </v-list>
         </v-navigation-drawer>
 
-        <v-toolbar class="indigo" dark fixed>
+        <v-toolbar fixed class="cyan" dark>
             <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
             <v-toolbar-title>Toolbar</v-toolbar-title>
         </v-toolbar>
 
         <main>
-        <v-container fluid>
-            <!--v-router-->
-            <router-view></router-view>
-        </v-container>
+            <v-container fluid>
+                <!--v-router-->
+                <router-view></router-view>
+            </v-container>
         </main>
 
-        <v-footer class="indigo">
+        <v-footer class="cyan">
             <span class="white--text">© 2017</span>
         </v-footer>
     </v-app>
@@ -42,7 +54,13 @@
         name: 'app',
         data () {
             return {
-                drawer: true
+                drawer: null,
+                items: [
+                    { title: 'Home', icon: 'dashboard' },
+                    { title: 'About', icon: 'question_answer' }
+                ],
+                mini: false,
+                right: null
             }
         }
     }
