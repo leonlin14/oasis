6e94c6d2453d7c01931b5de68d5145adb2fc1b11
leonlin <leonlin14@gmail.com>
2017-09-23 11:18:09 +0800

    update layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,49 @@
 <template>
-    <div id="app">
-        <router-view></router-view>
-    </div>
+    <v-app id="app" toolbar footer>
+        <v-navigation-drawer
+            persistent
+            v-model="drawer"
+            light
+            enable-resize-watcher
+            absolute
+        >
+            <v-list dense>
+                <v-list-tile @click="">
+                    <v-list-tile-action>
+                        <v-icon>home</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>Home</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-toolbar class="indigo" dark fixed>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title>Toolbar</v-toolbar-title>
+        </v-toolbar>
+
+        <main>
+        <v-container fluid>
+            <!--v-router-->
+            <router-view></router-view>
+        </v-container>
+        </main>
+
+        <v-footer class="indigo">
+            <span class="white--text">© 2017</span>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
-    export default { name: 'app' }
+    export default {
+        name: 'app',
+        data () {
+            return {
+                drawer: true
+            }
+        }
+    }
 </script>
